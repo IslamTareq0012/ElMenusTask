53f01d0ece6482ea963a00741bfa958695f4a085
Islam-tareq <islam_elmasry@hotmail.com>
2021-10-14 09:30:13 +0200

    Add project files.

---
+++ Orders/Order.Aggregate/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Order.Aggregate
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int BuyerID { get; set; }
+
+        public decimal OrderTotalValue { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+
+    }
+}
---
+++ Orders/Order.Aggregate/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Order.Aggregate
+{
+    public class OrderItem
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public bool  isValid { get; set; }
+        public decimal ItemPrice { get; set; }
+    }
+}
