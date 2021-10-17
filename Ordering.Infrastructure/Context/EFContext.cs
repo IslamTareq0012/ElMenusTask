9e94e6c055baca59c6deb5a46a27ad1c958919bd
Islam tareq <islam.tareq@waffarx.com>
2021-10-17 10:43:49 +0200

    Seeding data

--- Ordering.Infrastructure/Context/EFContext.cs
+++ Ordering.Infrastructure/Context/EFContext.cs
@@ -18,7 +18,7 @@ namespace Ordering.Infrastructure.Context
         public EFContext(DbContextOptions<EFContext> options)
             : base(options)
         {
-                
+
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -43,6 +43,27 @@ namespace Ordering.Infrastructure.Context
                 .HasOne<OrderItem>(sc => sc.OrderItem)
                 .WithMany(s => s.OrderItemToOrders)
                 .HasForeignKey(sc => sc.OrderItemID);
+
+            //Seeding data with 3 Items
+
+            modelBuilder.Entity<OrderItem>().HasData(
+                    new OrderItem
+                    {
+                        isValid = true,
+                        ItemPrice = 100,
+                        Name = "Item1",
+                    }, new OrderItem
+                    {
+                        isValid = true,
+                        ItemPrice = 100,
+                        Name = "Item2",
+                    }, new OrderItem
+                    {
+                        isValid = true,
+                        ItemPrice = 100,
+                        Name = "Item3",
+                    }
+                );
 
         }
 
